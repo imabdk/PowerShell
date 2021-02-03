875bda48683805ad80396a677362fb9e3ec33eeb
Martin Bengtsson <mab@imab.dk>
2021-02-03 13:24:24 +0100

    Add files via upload

---
+++ Detect-LowDiskSpace.ps1
@@ -0,0 +1,10 @@
+﻿$os = Get-CimInstance Win32_OperatingSystem
+$systemDrive = Get-CimInstance Win32_LogicalDisk -Filter "deviceid='$($os.SystemDrive)'"
+if (($systemDrive.FreeSpace/$systemDrive.Size) -le '0.70') {
+    Write-Output "Disk space is considered low. Script is exiting with 1 indicating error"
+    exit 1
+}
+else {
+    Write-Output "Disk space is OK."
+    exit 0
+}
