881012501c7c698cc416ff6120ecf87014b4751f
Martin Bengtsson <mab@imab.dk>
2021-08-17 22:41:44 +0200

    Delete giggle.ps1

--- giggle.ps1
+++
@@ -1,6 +0,0 @@
-﻿Add-Type -AssemblyName PresentationCore,PresentationFramework
-$ButtonType = [System.Windows.MessageBoxButton]::YesNo
-$MessageIcon = [System.Windows.MessageBoxImage]::Error
-$MessageBody = "Are you sure you want to delete the entire system??"
-$MessageTitle = "Confirm Deletion"
-$Result = [System.Windows.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
